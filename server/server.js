eabc9f022def0d096a7754d27795fb98b671da77
2k23Gautam <202301214@dau.ac.in>
2026-04-08 16:56:02 +0530

    deployement ready

--- server/server.js
+++ server/server.js
@@ -44,4 +44,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(distPath, 'index.html'));
 });
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
